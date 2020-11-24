08a128b3b986020a9524851acc03f57575ca4e31
liushooter <shooter.hero@gmail.com>
2020-11-25 03:58:49 +0800

    mintMulti

--- contracts/ITaowa.sol
+++ contracts/ITaowa.sol
@@ -6,7 +6,7 @@ interface ITaowa {
     function create(string memory name, string memory symbol, address[] memory _materials, uint[] memory _amounts) external returns (address);
 
     // 多个ERC20代币合成一个自定义代币
-    // function mintMulti(address erc20Addr, address[] calldata _preToken, uint256[] calldata _bassetAmount) external returns (uint256 massetMinted);
+    function mintMulti(address erc20Addr, address[] calldata tokens, uint256[] calldata amounts) external returns (uint256 massetMinted);
 
     // 一个自定义代币赎回对应的多个ERC20
     // function redeem(address _targetAsset, uint256 _bassetAmount) external returns (uint256 massetRedeemed);
--- contracts/Taowa.sol
+++ contracts/Taowa.sol
@@ -3,6 +3,7 @@ pragma experimental ABIEncoderV2;
 
 import { ITaowa } from "./ITaowa.sol";
 import { CustomToken } from "./CustomToken.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Taowa is ITaowa{
 
@@ -13,6 +14,9 @@ contract Taowa is ITaowa{
     // 创建事件
     event Created(string name, string symbol, uint256 supply, address tokenAddr);
 
+    // 铸造事件
+    event MintMulti(address tokenAddr, uint256 supply);
+
     function create(string memory _name, string memory _symbol, address[] memory _materials, uint[] memory _amounts) override external returns(address) {
         CustomToken token = new CustomToken(_name, _symbol);
         token.fuse(_materials, _amounts);
@@ -22,6 +26,33 @@ contract Taowa is ITaowa{
         groupsLength +=1;
         emit Created(_name, _symbol, 0, addr);
         return addr;
+    }
+
+    // 合成
+    function mintMulti(
+        address erc20Addr,
+        address[] calldata _tokens,
+        uint256[] calldata _amounts
+    )
+        override external
+        returns (uint256)
+    {
+        uint256 len = _tokens.length;
+        require(len > 0, "_tokens length must > 0");
+        require(len == _amounts.length, "lenght must be equal");
+
+        // 将资产划转到合约
+        for(uint256 i = 0; i < len; i++) {
+            address addr = _tokens[i];
+
+            ERC20 token = ERC20(addr);
+            token.transferFrom(msg.sender, address(this), _amounts[i]); // safeTransferFrom
+        }
+
+        emit MintMulti(erc20Addr, 1e18);
+
+        CustomToken(erc20Addr).mint(msg.sender, 1e18);
+        return 1e18;
     }
 
     function getTokenList() public view returns(address[] memory) {
